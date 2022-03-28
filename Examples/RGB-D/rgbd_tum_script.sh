32ca8e700987c8b683d044d8959f7cbdca67e14d
WFram <earl.freedom.ea@gmail.com>
2022-03-29 00:44:47 +0300

    init

---
+++ Examples/RGB-D/rgbd_tum_script.sh
@@ -0,0 +1,31 @@
+# For running only from project directory
+#Examples/RGB-D/rgbd_tum_seg Vocabulary/ORBvoc.txt \
+#           Examples/RGB-D/TUM3.yaml \
+#           /home/wfram/Downloads/rgbd_dataset_freiburg3_walking_xyz \
+#           /home/wfram/Downloads/rgbd_dataset_freiburg3_walking_xyz/associate.txt \
+#           prototxts/segnet_pascal.prototxt \
+#           /home/wfram/Downloads/segnet_pascal.caffemodel \
+#           LUT/pascal.png \
+#           tum_xyz
+
+#Examples/RGB-D/rgbd_tum_seg Vocabulary/ORBvoc.txt \
+#           Examples/RGB-D/TUM3.yaml \
+#           /home/wfram/Datasets/rgbd_dataset_freiburg3_sitting_xyz \
+#           /home/wfram/Datasets/rgbd_dataset_freiburg3_sitting_xyz/associationsSemantic.txt
+
+#Examples/RGB-D/rgbd_tum_seg Vocabulary/ORBvoc.txt \
+#           Examples/RGB-D/TUM3.yaml \
+#           /home/wfram/Datasets/rgbd_dataset_freiburg3_walking_halfsphere \
+#           /home/wfram/Datasets/rgbd_dataset_freiburg3_walking_halfsphere/associationsSemantic.txt
+
+#Examples/RGB-D/rgbd_tum_seg Vocabulary/ORBvoc.txt \
+#           Examples/RGB-D/TUM3.yaml \
+#           /home/wfram/Datasets/rgbd_dataset_freiburg3_walking_static \
+#           /home/wfram/Datasets/rgbd_dataset_freiburg3_walking_static/associationsSemantic.txt
+
+Examples/RGB-D/rgbd_tum_seg Vocabulary/ORBvoc.txt \
+           Examples/RGB-D/TUM3.yaml \
+           /home/wfram/Datasets/rgbd_dataset_freiburg3_walking_xyz \
+           /home/wfram/Datasets/rgbd_dataset_freiburg3_walking_xyz/associationsSemantic.txt
+
+# 1. XYZ; halfsphere, sitting
